40af4e2b9378a80af900b9df3eb530a93745460d
Jai Sharma <jai.sharma9782@gmail.com>
2020-10-02 08:41:44 +0530

    Task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LoadScript, GoogleMap, Marker, Polyline, DistanceMatrixService } from '@react-google-maps/api';
 
-function App() {
+const MapContainer = () => {
+
+  const mapStyles = {
+    height: "80vh",
+    width: "80%"
+  }
+
+  const pathCoordinates = [
+    {
+      name: "Vivek Vihar",
+      location: {
+        lat: 26.891394,
+        lng: 75.768275
+      },
+    },
+    {
+      name: "Techno Softwares",
+      location: {
+        lat: 26.789785,
+        lng: 75.830513
+      },
+    },
+  ];
+
+  const defaultCenter = {
+    lat: 26.891394, lng: 75.768275
+  }
+
+  // const Haversine_Distance = (mk1, mk2) => {
+
+  //   var R = 6371.0710;
+  //   var rlat1 = mk1.position.lat() * (Math.PI / 180);
+  //   var rlat2 = mk2.position.lat() * (Math.PI / 180);
+  //   var difflat = rlat2 - rlat1;
+  //   var difflon = (mk2.position.lng() - mk1.position.lng()) * (Math.PI / 180);
+
+  //   var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
+
+  //   return d;
+
+  // }
+
+  // const Distance = () => {
+  //   var distance = Haversine_Distance(mk1, mk2);
+  //   document.getElementById('msg').innerHTML = "Distance between markers: " + distance.toFixed(2) + " Km.";
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoadScript
+      googleMapsApiKey='AIzaSyBIol8Iw52VGQR5wOWYkAqcFeD4-7WQdPw'>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+      >
+        {
+          pathCoordinates.map(item => {
+            return (
+              <Marker position={item.location} />
+            )
+          })
+        }
+        <Polyline path={[{ lat: 26.891394, lng: 75.768275 }, { lat: 26.789785, lng: 75.830513 }]} ></Polyline>
+        <DistanceMatrixService
+          options={{
+            destinations: [{ lat: 26.891394, lng: 75.768275 }],
+            origins: [{ lat: 26.789785, lng: 75.830513 }],
+            travelMode: "DRIVING",
+          }}
+          callback={(response) => { console.log(response) }}
+        />
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default App;
+export default MapContainer;
